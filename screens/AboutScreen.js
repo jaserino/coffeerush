e486d1df356665f0b97097d13dc8fdaa79f48222
jaserino <jaserino1@gmail.com>
2022-12-01 18:26:30 -0500

    Temp profile page build

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export const AboutScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>About Us</Text>
+      <Text>Bob is a nerd</Text>
     </View>
   );
 };
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,87 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TextInput } from 'react-native';
 
 export const ProfileScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>My Profile here</Text>
+    <View style={{ width: '100%', height: '100%', backgroundColor: '#FFEBC1' }}>
+      <View style={{ padding: 20, alignItems: 'center' }}>
+        <Image
+          source={{
+            uri: 'https://images.pexels.com/photos/3777943/pexels-photo-3777943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          }}
+          style={{
+            borderRadius: 100,
+            borderWidth: 5,
+            borderColor: '#A64B2A',
+            width: 150,
+            height: 150,
+            marginTop: 30,
+            color: '#A64B2A',
+          }}
+        />
+      </View>
+      <View style={{ padding: 15 }}>
+        <View>
+          <Text style={{ opacity: 0.5, marginBottom: 6, color: '#A64B2A' }}>
+            Name
+          </Text>
+          <TextInput
+            placeholder="John Smith"
+            style={{
+              fontSize: 20,
+              padding: 10,
+              borderRadius: 10,
+              borderWidth: 1,
+              borderColor: '#8E3200',
+            }}
+          ></TextInput>
+        </View>
+        <View>
+          <Text style={{ opacity: 0.5, marginBottom: 6, color: '#A64B2A' }}>
+            Email
+          </Text>
+          <TextInput
+            placeholder="JohnSm88@aol.com"
+            style={{
+              fontSize: 20,
+              padding: 10,
+              borderRadius: 10,
+              borderWidth: 1,
+              borderColor: '#8E3200',
+            }}
+          ></TextInput>
+        </View>
+        <View>
+          <Text style={{ opacity: 0.5, marginBottom: 6, color: '#A64B2A' }}>
+            Phone
+          </Text>
+          <TextInput
+            placeholder="xxx-xxx-xxxx"
+            style={{
+              fontSize: 20,
+              padding: 10,
+              borderRadius: 10,
+              borderWidth: 1,
+              borderColor: '#8E3200',
+            }}
+          ></TextInput>
+        </View>
+        <View>
+          <Text style={{ opacity: 0.5, marginBottom: 6, color: '#A64B2A' }}>
+            Favorite Order
+          </Text>
+          <TextInput
+            placeholder="Whats your favorite order?"
+            style={{
+              fontSize: 20,
+              padding: 10,
+              borderRadius: 10,
+              borderWidth: 1,
+              borderColor: '#8E3200',
+            }}
+          ></TextInput>
+        </View>
+      </View>
     </View>
   );
 };
